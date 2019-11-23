20ca4ca6d73b1dcaf1cb414bbed0292a141d7307
Koeh <johnkoeh@gmail.com>
2019-11-22 23:27:22 -0800

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,50 +1,89 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./styles/app.module.css";
 import Header from "./components/Header";
 import background from "./images/james-pond-1qkyck-UL3g-unsplash.jpg";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import { useMediaQuery } from "react-responsive";
 
-const App = () => {
-  const isDesktopOrLaptop = useMediaQuery({ minWidth: 900 });
-  const display = isDesktopOrLaptop
-    ? "justify-content-between"
-    : "justify-content-start";
-  console.log(display);
-  return (
-    <div className={styles.app}>
-      <Container
-        fluid={true}
-        className={`d-flex flex-column ${display}`}
-        style={{ height: "50%" }}
-      >
-        <Row>
-          <Header className={styles.header} />
-        </Row>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectTypeSelected: ""
+    };
+  }
+  setProjectType(projectTypeSelected) {
+    this.setState({ projectTypeSelected });
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <Container
+          fluid={true}
+          className="d-flex flex-column justify-content-start"
+        >
+          <Row>
+            <Header className={styles.header} />
+          </Row>
 
-        <Row></Row>
-        <Row>
-          <div className={styles.title}>Software Engineer.</div>
-        </Row>
-        <Row className={styles.subTitles}>
-          <Col></Col>
-          <Col className={styles.subTitle} xs={1}>
-            JS
-          </Col>
-          <Col className={styles.subTitle} xs={1}>
-            Python
-          </Col>
-          <Col className={styles.subTitle} xs={1}>
-            C#
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
-      <img className={styles.background} src={background} alt="Idea" />
-    </div>
-  );
-};
+          <Row></Row>
+          <Row>
+            <div className={styles.title}>
+              {this.state.projectTypeSelected} Engineer.
+            </div>
+          </Row>
+          <Row className={styles.subTitles}>
+            <Col></Col>
+            <Col
+              className={styles.subTitle}
+              xs={1}
+              onClick={() => this.setProjectType("ML")}
+            >
+              ML
+            </Col>
+
+            <Col
+              className={styles.subTitle}
+              xs={1}
+              onClick={() => this.setProjectType("Front End")}
+            >
+              Front End
+            </Col>
+            <Col
+              className={styles.subTitle}
+              xs={1}
+              onClick={() => this.setProjectType("Back End")}
+            >
+              Back End
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+
+        <div className={styles.projectList}>
+          <div className={styles.ticTacToe}></div>
+          <div>
+            <h2>Tic-Tac-Toe</h2>
+            <p>
+              Try your skill with this simple tic-tac-toe build purely with
+              react
+            </p>
+            <p>
+              <a
+                className="btn btn-secondary"
+                href="http://johann-tic-tac-toe.s3-website.us-east-2.amazonaws.com/"
+              >
+                View Detail
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <img className={styles.background} src={background} alt="Idea" />
+      </div>
+    );
+  }
+}
 
 export default App;
